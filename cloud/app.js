2b170c4a7d16c1c48c13467a83faa83dd0ff4c2d
lakshmana <lakshman.chimata@gmail.com>
2018-06-05 10:13:29 +0530

    serving static pages with express

--- cloud/app.js
+++ cloud/app.js
@@ -8,10 +8,11 @@ var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var usersv1 = require('./api/v1/controllers/users')
 var adminv1 = require('./api/v1/controllers/admin')
-
+var connect = require('connect');
+var serveStatic = require('serve-static');
 var usersv2 = require('./api/v2/controllers/users')
 var adminv2 = require('./api/v2/controllers/admin')
-
+var path = require('path');
 var configuration = require('./config/config')
 var database = require('./models/database')
 
@@ -22,6 +23,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 5857;        // set our port
+
+var distName = "webapp/dist/webapp"
+// connect().use(serveStatic(__dirname)).listen(5857, function(){
+//     console.log('Server running on 8080...');
+// });
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -37,9 +43,9 @@ var options = {
     ]
 }
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
-});
+// router.get('/', function (req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' });
+// });
 
 // more routes for our API will happen here
 
@@ -50,6 +56,7 @@ app.use('/api/v1/users', usersv1);
 app.use('/api/v1/admin', adminv2);
 app.use('/api/v2/users', usersv2);
 app.use('/api/v2/admin', adminv2);
+app.use(express.static(path.join(__dirname,distName))); 
 database.connectDB(configuration.dbUrl, configuration.dbName)
 
 setTimeout(function () {
